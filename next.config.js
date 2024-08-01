72f16e1a344385e8bd4a68d99a21978c87f00260
sudh-202 <srivastavasudhanshu202@gmail.com>
2024-08-01 12:11:38 +0530

    final update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: { unoptimized: true }
-};
-
-export default nextConfig;
+    images: { unoptimized: true },
+  };
+  
+  module.exports = nextConfig;
+  
