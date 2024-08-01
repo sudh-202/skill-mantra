5ece6fdc5c6d03dba5da27274bae917a5bd80809
sudh-202 <srivastavasudhanshu202@gmail.com>
2024-08-01 18:19:24 +0530

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/ui/button";
 
 const Hero = () => {
     return (
-        <section className="max-container padding-container flex flex-col lg:gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row bg-[#21355C]">
+        <section className="max-container padding-container flex flex-col lg:gap-20 xl:gap-24 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row bg-[#21355C]">
              
             <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
             
@@ -35,14 +35,14 @@ const Hero = () => {
                 <div className="flex flex-wrap gap-4 md:gap-10 mt-6 flex-col lg:flex-row">
                     {data.stats.map((stat, index) => (
                         <div key={index} className="w-full md:w-auto text-start mb-2 md:mb-0 flex lg:flex-col gap-4 lg:gap-2">
-                            <div className="text-xl md:text-3xl lg:text-5xl font-bold text-white">{stat.percentage}</div>
+                            <div className="text-xl md:text-3xl lg:text-4xl font-bold text-white">{stat.percentage}</div>
                             <div className="text-sm md:text-base lg:text-lg text-white">{stat.description}</div>
                             <Image
                                 src="/icons/arrow.webp"
                                 alt="circle"
                                 width={40}
                                 height={100}
-                                className="absolute right-[-15px] top-[330px] z-0 "
+                                className="absolute right-[-65px] top-[350px] z-0 "
                             />
                         </div>
                     ))}
