ea4f48bc62cbc670625850f8f07fd4644c1be1c6
sudh-202 <srivastavasudhanshu202@gmail.com>
2024-08-08 17:14:28 +0530

    new changes

--- components/Faqs.tsx
+++ components/Faqs.tsx
@@ -9,7 +9,7 @@ import { faqs } from "@/constants"
 export function FAQS() {
     return (
         <section className=" flex justify-center flex-col items-center text-black py-20 px-10 md:px-0 bg-[#BDFFFC]">
-            <h2 className="text-center text-4xl lg:text-5xl font-abril ml-4 font-semibold text-black mb-8">Frequently Asked Questions</h2>
+            <h2 className="text-center text-xl lg:text-5xl font-abril ml-4 font-semibold text-black mb-8">Frequently Asked Questions</h2>
             <Accordion type="single" collapsible className="w-full text-xl flex justify-center  flex-col md:px-48">
                 {faqs.map((faq) => (
                     <AccordionItem key={faq.id} value={faq.id} className="py-3 border-b-2 border-gray-400">
