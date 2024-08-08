a71a540c2d9bf1dbec425cec29221dfafe48af35
sudh-202 <srivastavasudhanshu202@gmail.com>
2024-08-08 17:04:43 +0530

    this is final update for homepage

--- components/Faqs.tsx
+++ components/Faqs.tsx
@@ -8,11 +8,11 @@ import { faqs } from "@/constants"
 
 export function FAQS() {
     return (
-        <section className="max-w-5xl mx-auto text-white py-20 px-10 md:px-0">
-            <h2 className="text-xl text-center md:text-3xl lg:text-5xl font-abril ml-4 font-semibold text-white mb-8">Frequently Asked Questions</h2>
-            <Accordion type="single" collapsible className="w-full text-xl ">
+        <section className=" flex justify-center flex-col items-center text-black py-20 px-10 md:px-0 bg-[#BDFFFC]">
+            <h2 className="text-xl text-center md:text-3xl lg:text-5xl font-abril ml-4 font-semibold text-black mb-8">Frequently Asked Questions</h2>
+            <Accordion type="single" collapsible className="w-full text-xl flex justify-center  flex-col px-48">
                 {faqs.map((faq) => (
-                    <AccordionItem key={faq.id} value={faq.id} className="py-3">
+                    <AccordionItem key={faq.id} value={faq.id} className="py-3 border-b-2 border-gray-400">
                         <AccordionTrigger>{faq.question}</AccordionTrigger>
                         <AccordionContent>{faq.answer}</AccordionContent>
                     </AccordionItem>
