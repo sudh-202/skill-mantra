0db448645e441d80b615231525bd5dd2660268aa
sudh-202 <srivastavasudhanshu202@gmail.com>
2024-08-21 15:38:36 +0530

    the changes

--- app/counselling/page.tsx
+++ app/counselling/page.tsx
@@ -16,17 +16,17 @@ const Counseliing = () => {
                 <h2 className="text-[4vw] font-bold text-center text-gray-800 mb-8">
                     What is covered in the counselling session:
                 </h2>
-                <div className="flex flex-wrap justify-center gap-6">
+                <div className="flex flex-wrap justify-center gap-8">
                     {cardsData.map((card, index) => (
                         <div
                             key={index}
                             className="flex flex-col items-center bg-blue-90 rounded-lg shadow-lg overflow-hidden w-full sm:w-1/2 lg:w-1/4 p-4"
                         >
-                            <div className="w-full h-72 flex items-center justify-center text-white text-6xl">
+                            <div className="w-full h-72 flex items-center justify-center text-white text-[6vw]">
                                 <card.icon />
                             </div>
                             <div className="mt-4">
-                                <h3 className="text-[8vw] font-semibold text-center text-white">{card.title}</h3>
+                                <h3 className="text-[2vw] font-semibold text-center text-white">{card.title}</h3>
                                 <p className="text-sm text-center text-gray-300 mt-2 whitespace-pre-line">
                                     {card.description}
                                 </p>
