0bed2097efd7d3a869efc4a3ddba96e5b7677d39
sudh-202 <srivastavasudhanshu202@gmail.com>
2024-08-23 12:04:15 +0530

    the changes

--- components/Courses/Shopsy.tsx
+++ components/Courses/Shopsy.tsx
@@ -7,7 +7,7 @@ const Shopsy = () => {
             <h2 className="text-black font-medium text-[3vw] text-center leading-[3vw]">Our </h2>
             <h3 className="text-blue-90 font-bold text-[5vw] text-center leading-[7vw]">Finance Partner</h3>
             <Image src="../courses/shopselogo.webp" alt="shopsylogo" width={500} height={140} className="object-contain py-5"  loading='lazy'/>
-            <p className="py-[2vw] px-[15vw] font-semibold">With Our Finance Partner, You Can Now Enjoy Easy And Affordable Payment Options, Making Quality Education More Accessible Than Ever Which Ensures Financial Flexibility Without Any Hassle. Discover The Benefits Of Our New EMI Plans And Take The Next Step In Your Learning Journey With Confidence.​</p>
+            <p className="py-[2vw] px-[15vw] font-semibold capitalize">With Our Finance Partner, You Can Now Enjoy Easy And Affordable Payment Options, Making Quality Education More Accessible Than Ever Which Ensures Financial Flexibility Without Any Hassle. Discover The Benefits Of Our New EMI Plans And Take The Next Step In Your Learning Journey With Confidence.​</p>
             </div>
         </main>
      );
--- components/Courses/Who.tsx
+++ components/Courses/Who.tsx
@@ -5,7 +5,7 @@ const WhyCourse = () => {
           <p className="font-semibold md:p-0 p-[10vw] md:text-left text-center">At Skill Mantra, we are dedicated to empowering individuals with the practical skills and knowledge needed to excel in today’s competitive job market. Specialising in short-term, job-oriented courses, we bridge the gap between academic learning and real-world demands. Our mission is to equip our students with industry-relevant expertise, ensuring they are job-ready from day one. With a focus on fields like accounting, HR, tax, and financial modelling, we provide hands-on training and career support to help you succeed in your chosen profession.</p>
           <div className="flex flex-col justify-center items-center">
           <Image src="../courses/who.webp" alt="who" width={1200} height={100}/>
-          <p className="md:text-[2.5vw] text-blue-90 text-[12vw]">WHO WE ARE</p>
+          <p className="md:text-[2.5vw] text-blue-90 text-[12vw]">WHO ARE WE</p>
           </div>
         </main>
      );
