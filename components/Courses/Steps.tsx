7cb3ac971ed8e0bf001e7c9d9c40fb235b73d8df
sudh-202 <srivastavasudhanshu202@gmail.com>
2024-08-27 17:23:48 +0530

    the changes

--- components/Courses/Steps.tsx
+++ components/Courses/Steps.tsx
@@ -5,12 +5,12 @@ import Image from "next/image";
 
 const Steps: React.FC = () => {
     return (
-        <main className="flex flex-col justify-around items-center bg-white   px-[6vw] md:h-[100vh]">
-            <h1 className="md:text-[4vw] tracking-tight text-[7vw] font-semibold text-blue-90 pb-[2vw]">Program Cycle</h1>
-            <h2 className=" md:text-[2vw] text-[4vw]  text-blue-90 text-center tracking-tight">
+        <main className="flex flex-col items-center bg-white py-[6vw]  px-[6vw] ">
+            <h1 className="md:text-[4vw] tracking-tight text-[7vw] font-semibold text-blue-90 pb-[30px]">Program Cycle</h1>
+            <h2 className=" md:text-[2vw] text-[4vw]  text-blue-90 text-center tracking-tight pb-[30px]">
                 There are 3 simple steps in the Program Cycle which are detailed below:
             </h2>
-            <Image src="/courses/1.webp" alt="Steps"  width={100} height={100} className="w-[60vw]  "/>
+            <Image src="/courses/1.webp" alt="Steps"  width={100} height={100} className="w-[50vw]  "/>
             {/* <div className="flex  flex-row items-center justify-center space-y-6 md:space-y-0 md:space-x-12 -space-x-4 md:mt-8 gap-10">
                 {stepsData.map((stepData, index) => (
                     <div key={index} className="flex flex-col  items-center text-center">
