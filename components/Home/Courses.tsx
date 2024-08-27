e9069f91cabdd1f6528250ad0da9499bb4342ecc
sudh-202 <srivastavasudhanshu202@gmail.com>
2024-08-27 15:57:14 +0530

    the changes

--- components/Home/Courses.tsx
+++ components/Home/Courses.tsx
@@ -1,9 +1,9 @@
-"use client"
-import React, { useState } from 'react';
-import Image from 'next/image';
-import { courses } from '@/constants';
+"use client";
+import React, { useState, useRef } from "react";
+import Image from "next/image";
+import { courses } from "@/constants";
 import { Button } from "@/components/ui/button";
-import Link from 'next/link';
+import Link from "next/link";
 import {
     Carousel,
     CarouselContent,
@@ -11,57 +11,54 @@ import {
     CarouselNext,
     CarouselPrevious,
 } from "@/components/ui/carousel";
-import { FaClock, FaGraduationCap, FaUniversity, FaDollarSign, FaCheckCircle } from 'react-icons/fa';
+import { FaClock, FaGraduationCap, FaUniversity, FaCheckCircle } from "react-icons/fa";
 import { motion, useScroll, useTransform, useMotionValueEvent } from "framer-motion";
-import { useRef } from "react"
 
 const CoursesSection: React.FC = () => {
     const [activeIndex, setActiveIndex] = useState(0);
     const heroRef = useRef(null);
     const { scrollYProgress } = useScroll({
       target: heroRef,
-      offset: ['start end', 'end start']
+      offset: ["start end", "end start"],
     });
     const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
-    useMotionValueEvent(scrollYProgress, "change", (latestValue) => console.log(latestValue))
+    useMotionValueEvent(scrollYProgress, "change", (latestValue) =>
+      console.log(latestValue)
+    );
 
     return (
-        <div className="w-full py-16  text-blue-900 flex flex-col items-center md:px-12 px-10 bg-blue-90 border-b-2 border-white">
-            <h2 className="text-center lg:text-7xl text-4xl  mb-8 text-white">Our Courses</h2>
+        <div className="w-full py-16 text-blue-900 flex flex-col items-center md:px-12 px-10 bg-blue-90 border-b-2 border-white">
+            <h2 className="text-center lg:text-7xl text-4xl mb-8 text-white">Our Courses</h2>
             <div className="relative w-full max-w-[1320px] pb-24">
-                <Carousel
-                    opts={{ align: "start" }}
-                    className="w-full"
-                >
-                    <CarouselContent className=''>
+                <Carousel opts={{ align: "start" }} className="w-full">
+                    <CarouselContent>
                         {courses.map((course, index) => (
-                            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3 w-full ">
+                            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3 w-full">
                                 <div className="bg-[#FFC224] py-8 px-3 rounded-2xl shadow-md flex flex-col h-full">
-                                    <div className='bg-white  p-6 mb-4'>
-                                        <h3 className="md:text-2xl text-2xl font-semibold mb-2 border-b-2 border-blue-90 pb-4">{course.title}</h3>
-
+                                    <div className="bg-white p-6 mb-4">
+                                        <h3 className="md:text-2xl text-2xl font-semibold mb-2 border-b-2 border-blue-90 pb-4">
+                                            {course.title}
+                                        </h3>
                                         <div className="mb-2 flex flex-row justify-between border-b-2 border-blue-90 py-3">
-                                            <div className='flex flex-col'>
-                                                
-                                                <p className=' font-bold flex items-center'>
-                                                <FaClock className="mr-2" />
+                                            <div className="flex flex-col">
+                                                <p className="font-bold flex items-center">
+                                                    <FaClock className="mr-2" />
                                                     Duration
                                                 </p>
                                                 {course.duration}
-                                                <p className='font-bold flex items-center mt-4 '>
+                                                <p className="font-bold flex items-center mt-4">
                                                     <FaGraduationCap className="mr-2" />
                                                     Modules
                                                 </p>
                                                 {course.modules}
                                             </div>
-                                            <div className='flex flex-col'>
-                                                <p className='font-bold flex items-center'>
+                                            <div className="flex flex-col">
+                                                <p className="font-bold flex items-center">
                                                     <FaUniversity className="mr-2" />
                                                     Eligibility
                                                 </p>
                                                 {course.eligibility}
-                                                <p className='font-bold flex items-center  mt-4'>
-                                                    {/* <FaDollarSign className="mr-2" /> */}
+                                                <p className="font-bold flex items-center mt-4">
                                                     ₹ Mode
                                                 </p>
                                                 {course.mode}
@@ -77,10 +74,10 @@ const CoursesSection: React.FC = () => {
                                         </ul>
                                     </div>
                                     <div className="flex justify-center gap-5 mt-auto">
-                                        <Button asChild className='bg-blue-90 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'>
+                                        <Button asChild className="bg-blue-90 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
                                             <Link href="/courses">Learn More</Link>
                                         </Button>
-                                        <Button asChild className=' bg-[#00E0D3] hover:bg-green-600 text-white font-bold py-2 px-4 rounded'>
+                                        <Button asChild className="bg-[#00E0D3] hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
                                             <Link href="/login">Enroll Now</Link>
                                         </Button>
                                     </div>
@@ -88,8 +85,8 @@ const CoursesSection: React.FC = () => {
                             </CarouselItem>
                         ))}
                     </CarouselContent>
-                    <CarouselPrevious className="block absolute  left-0 top-1/2 transform -translate-y-1/2 md:-translate-x-12 -translate-x-8 bg-transparent text-blue-90  border-transparent " />
-                    <CarouselNext className="block absolute right-0 top-1/2 transform -translate-y-1/2 md:translate-x-12 translate-x-10 bg-transparent text-blue-90  border-transparent" />
+                    <CarouselPrevious className="block absolute md:left-[-3vw] -left-[30px] top-1/2 transform -translate-y-1/2 bg-transparent text-white border-transparent z-40" />
+                    <CarouselNext className="block absolute md:right-[-3vw] top-1/2 transform -translate-y-1/2 bg-transparent text-white border-transparent z-40" />
                 </Carousel>
                 <motion.img
                     src="/icons/dotsv2.webp"
@@ -97,17 +94,9 @@ const CoursesSection: React.FC = () => {
                     width={250}
                     height={100}
                     className="absolute bottom-[-120px] left-0 z-10 hidden lg:block"
-                    loading='lazy'
-                    animate={{
-                        translateY: [-10, 10],
-                      }}
-                      transition={{
-                        repeat: Infinity,
-                        repeatType: "mirror",
-                        duration: 2,
-                        ease: "easeInOut",
-                      }}
-                    
+                    loading="lazy"
+                    animate={{ translateY: [-10, 10] }}
+                    transition={{ repeat: Infinity, repeatType: "mirror", duration: 2, ease: "easeInOut" }}
                 />
             </div>
         </div>
