9bdd52e03780fc2ad940dce2efdaac7db1c3b1bc
sudh-202 <srivastavasudhanshu202@gmail.com>
2024-08-28 12:00:00 +0530

    the update

--- components/Courses/CarouselIconSection.tsx
+++ components/Courses/CarouselIconSection.tsx
@@ -18,17 +18,19 @@ const CarouselIconSection: FC<CarouselIconSectionProps> = ({ courseSlug }) => {
     if (item.title === "Program Structure") {
       return {
         ...item,
-        description: `A ${durationMap[courseSlug] || "30+"} hour journey of focused learning through theory and practice.`,
+        description: `A ${
+          durationMap[courseSlug] || "30+"
+        } hour journey of focused learning through theory and practice.`,
       };
     }
     return item;
   });
 
   return (
-    <section className="py-12 overflow-hidden border-b-2 border-blue-10 bg-[url('/courses/lab.webp')] bg-no-repeat bg-cover bg-center bg-opacity-50">
+    <section className="py-12 overflow-hidden border-b-2 border-blue-10 bg-[url('/courses/lab.webp')] bg-no-repeat bg-cover bg-center bg-opacity-50 relative">
       <div className="container mx-auto md:px-[2vw]">
         <Carousel>
-          <CarouselContent className="">
+          <CarouselContent className="relative">
             {updatedItems.map((item, index) => (
               <CarouselItem
                 key={index}
@@ -39,15 +41,19 @@ const CarouselIconSection: FC<CarouselIconSectionProps> = ({ courseSlug }) => {
                     <div className="mb-4 text-4xl text-white">
                       <item.icon className="w-[10vh] h-[10vh] bg-blue-90 rounded-full p-4" />
                     </div>
-                    <h3 className="text-[2vw] font-semibold pb-2">{item.title}</h3>
+                    <h3 className="text-[2vw] font-semibold pb-2">
+                      {item.title}
+                    </h3>
                     <p className="text-[1vw]">{item.description}</p>
                   </CardContent>
                 </Card>
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious className="md:w-[8vh] md:h-[8vh] border-[5px] border-white text-white" />
-          <CarouselNext className="md:w-[8vh] md:h-[8vh] border-[5px] border-white text-white" />
+
+          {/* Navigation Arrows */}
+          <CarouselPrevious className="absolute left-0 top-1/2 transform -translate-y-1/2 z-10 md:w-[8vh] md:h-[8vh] border-[5px] border-white text-white bg-blue-90 rounded-full flex items-center justify-center cursor-pointer" />
+          <CarouselNext className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 md:w-[8vh] md:h-[8vh] border-[5px] border-white text-white bg-blue-90 rounded-full flex items-center justify-center cursor-pointer" />
         </Carousel>
       </div>
     </section>
