717cf63912a48536d7d246e8956aaeb0c73c3143
sudh-202 <srivastavasudhanshu202@gmail.com>
2024-08-29 16:08:35 +0530

    the changes

--- components/Home/OurTrainer.tsx
+++ components/Home/OurTrainer.tsx
@@ -1,7 +1,7 @@
-import { FC } from 'react';
-import { trainers } from '@/constants';
+import { FC } from "react";
+import { trainers } from "@/constants";
 import { Card, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
-import Image from 'next/image';
+import Image from "next/image";
 import {
   Carousel,
   CarouselContent,
@@ -12,24 +12,30 @@ import {
 
 const TrainerCarousel: FC = () => {
   return (
-    <section className='bg-blue-90 text-white py-20 px-10 md:px-0'>
-      <h2 className='text-4xl md:text-5xl font-medium text-center mb-12'>Our Trainers</h2>
-      <div className="relative w-full max-w-7xl mx-auto">
-        <Carousel className='relative w-full'>
+    <section className="bg-blue-90 text-white py-20 px-10 md:px-0">
+      <h2 className="text-4xl md:text-5xl font-medium text-center mb-12">
+        Our Trainers
+      </h2>
+      <div className="w-full max-w-7xl mx-auto">
+        <Carousel className="w-full ">
           <CarouselContent>
             {trainers.map((trainer, index) => (
-              <CarouselItem key={index} className="basis-full md:basis-1/3 flex items-center justify-center ">
-                <Card className="relative flex flex-col items-center justify-between bg-[#00E0D3] text-black rounded-[40px] p-6 ">
-                  <CardHeader className="flex flex-col items-center text-center mb-4 ">
-                    <Image 
-                      src={trainer.image} 
-                      alt={trainer.name} 
-                      width={100} 
-                      height={100} 
-                      className="w-32 h-32 rounded-full object-cover  mb-4 border-2  " 
-                    />
-                    <CardTitle className="text-2xl font-bold">{trainer.name}</CardTitle>
-                  </CardHeader>
+              <CarouselItem
+                key={index}
+                className="basis-full md:basis-1/3 flex items-center justify-center"
+              >
+                <Card className=" flex flex-col  items-center justify-end bg-[#00E0D3] text-black rounded-[40px] p-6 ">
+                  <Image
+                    src={trainer.image}
+                    alt={trainer.name}
+                    width={130}
+                    height={130}
+                    className=" pb-4"
+                  />
+                  <CardTitle className="text-2xl font-bold">
+                    {trainer.name}
+                  </CardTitle>
+
                   <CardFooter className="flex flex-col items-center">
                     <p className="mb-2">{trainer.experience}</p>
                     <p className="mb-2">{trainer.qualification}</p>
